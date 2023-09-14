78b9ed7a3ae88e8be2f49642f83ad0d590238d25
Joram3000 <joramkroon@live.nl>
2023-09-14 11:30:41 +0200

    standard under construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 // import Navbar from "./components/navbar";
 // import Footer from "./components/footer/index";
-// import UnderConstructionPage from "./pages/UnderConstruction";
+import UnderConstructionPage from "./pages/UnderConstruction";
 
 function App() {
   return (
     <div className="layout">
-      <h1>Under construction</h1>
+      {/* <h1>Under construction</h1>
       <p>
         Wil je in contact komen met mij? Stuur een e-mail naar
         mirjam@kroontrainingadvies.com Of bel 0653507815
-      </p>
-      {/* <UnderConstructionPage /> */}
+      </p> */}
+      <UnderConstructionPage />
     </div>
   );
 }
--- src/pages/UnderConstruction/index.tsx
+++ src/pages/UnderConstruction/index.tsx
@@ -1,28 +1,60 @@
 import "./style.css";
+import mirjamfoto from "../../assets/mirjamfoto.jpeg";
 
 const UnderConstructionPage = () => {
   return (
     <div className="container">
-      <div className="mainImage"></div>
+      <div className="title">
+        <img src={mirjamfoto} className="mirjamFoto" />
+        <h1>Kroon training en advies</h1>
+      </div>
+      <div className="textBlock">
+        <h2>ONDER CONSTRUCTIE</h2>
+        <p>
+          Mijn site is onder constructie omdat ik aan een nieuwe site werk.
+          <br></br>Maar dat betekent niet dat ik “Under-construction” ben.
+        </p>
+        <p>
+          Ik ben volop bezig met het helpen ontwikkelen van mensen door middel
+          van trainingen, het verder helpen brengen van
+          samenwerkingsvraagstukken en individuele-en teamcoaching.
+        </p>
+        <p>
+          De planning is dat mijn nieuwe site eind oktober te bezoeken is. Nu is
+          er informatie te vinden op mijn LinkedIn profiel: Mirjam
+          Kroon-Hoekendijk Wil je in contact komen met mij?
+        </p>
+      </div>
 
-      <h1>Kroon training en advies</h1>
+      <a
+        className="linkedInBlock"
+        target="_blank"
+        href="https://www.linkedin.com/in/mirjam-kroon-hoekendijk"
+      >
+        <strong>Ga naar linked in</strong>
+      </a>
 
-      <p className="underContructionCopy">
-        Mijn site is onder constructie omdat ik aan een nieuwe site werk. Maar
-        dat betekent niet dat ik “Under-construction” ben. Ik ben volop bezig
-        met het helpen ontwikkelen van mensen door middel van trainingen, het
-        verder helpen brengen van samenwerkingsvraagstukken en individuele-en
-        teamcoaching. De planning is dat mijn nieuwe site eind oktober te
-        bezoeken is. Nu is er informatie te vinden op mijn LinkedIn profiel:
-        Mirjam Kroon-Hoekendijk Wil je in contact komen met mij?
-      </p>
-
-      <p className="howtoContact">
-        Stuur een e-mail naar mirjam@kroontrainingadvies.com Of bel 0653507815
-        Mirjam Kroon-Hoekendijk Trainer en (team-)coach Mirjam Kroon Training en
-        Advies KvK 23083220 BTW NL 001192641B33 Adres: Dorpsweg 71 4223 ND
-        Hoornaar (ZH)
-      </p>
+      <div className="textBlock">
+        <h2>CONTACTGEGEVENS</h2>
+        <p>
+          Wil je in contact komen met mij?<br></br>
+          Stuur een e-mail naar <strong>mirjam@kroontrainingadvies.com</strong>
+          <br></br>
+          Of bel: <strong>0653507815</strong>
+        </p>
+        <p>
+          Mirjam Kroon-Hoekendijk<br></br>
+          Trainer en (team-)coach Mirjam Kroon Training en Advies<br></br>
+          KvK 23083220<br></br>
+          BTW NL 00192641B33
+        </p>
+        <p>
+          <strong>Adres:</strong>
+          <br></br>
+          Dorpsweg 71<br></br>
+          4223 ND Hoornaar (ZH)
+        </p>
+      </div>
     </div>
   );
 };
