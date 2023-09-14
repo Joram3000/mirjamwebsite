ca468d9ddca85b4206e517f72405a88a19a883b8
Joram3000 <joramkroon@live.nl>
2023-09-14 14:52:27 +0200

    create basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import "./App.css";
 import Footer from "./components/footer/index";
 import Navbar from "./components/navbar";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
+import HomePage from "./pages/HomePage";
 import UnderConstructionPage from "./pages/UnderConstruction";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="layout">
-        <UnderConstructionPage />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<UnderConstructionPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </div>
+
+      {/* <div className="layout">
+        <UnderConstructionPage />
+      </div> */}
       <Footer />
     </>
   );
