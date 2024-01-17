3d1d138648b11169f0fa3d37c59589124d9d2285
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2024-01-17 13:01:35 +0100

    improve under construction

--- src/pages/UnderConstruction/index.tsx
+++ src/pages/UnderConstruction/index.tsx
@@ -1,18 +1,26 @@
 import "./style.css";
 import mirjamfoto from "../../assets/images/mirjam6.jpg";
-import { Anchor, Box, Container, Title, Text } from "@mantine/core";
+import {
+  Anchor,
+  Box,
+  Container,
+  Title,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 
 const UnderConstructionPage = () => {
+  const theme = useMantineTheme();
+
   return (
-    <Box>
+    <Box bg={theme.colors.custom[0]} c={theme.colors.custom[1]} p="md">
       <Container ta="center">
         <Box pt="md">
-          <img src={mirjamfoto} className="mirjamFoto" />
           <Title>Kroon Training & Advies</Title>
+          <img src={mirjamfoto} className="mirjamFoto" />
         </Box>
 
         <Box ta="center">
-          <Title order={2}>ONDER CONSTRUCTIE</Title>
           <Text>
             Mijn site is onder constructie omdat ik aan een nieuwe site werk.
             <br></br>Maar dat betekent niet dat ik “Under-construction” ben.
@@ -27,35 +35,13 @@ const UnderConstructionPage = () => {
             te vinden op mijn LinkedIn profiel:
           </Text>
           <Anchor
+            c={theme.colors.custom[1]}
             target="_blank"
             href="https://www.linkedin.com/in/mirjam-kroon-hoekendijk-0a99104/"
           >
             <strong>Ga naar LinkedIn</strong>
           </Anchor>
         </Box>
-
-        {/* <div>
-          <h2>CONTACTGEGEVENS</h2>
-          <p>
-            Wil je in contact komen met mij?<br></br>
-            Stuur een e-mail naar:
-            <strong> mirjam@kroontrainingadvies.com</strong>
-            <br></br>
-            Of bel: <strong>0653507815</strong>
-          </p>
-          <p>
-            Mirjam Kroon-Hoekendijk<br></br>
-            Trainer en (team-)coach Mirjam Kroon Training en Advies<br></br>
-            KvK 23083220<br></br>
-            BTW NL 00192641B33
-          </p>
-          <p>
-            <strong>Adres:</strong>
-            <br></br>
-            Dorpsweg 71<br></br>
-            4223 ND Hoornaar (ZH)
-          </p>
-        </div> */}
       </Container>
     </Box>
   );
